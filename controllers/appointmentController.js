a7b9f8fcf3c35ca4997a90d7a6a91ef9ccbf6cb6
MuktaHD <165270785+MuktaHD@users.noreply.github.com>
2024-06-23 22:19:29 +0530

    Update appointmentController.js

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -18,7 +18,7 @@ async function addAppointment(req, res) {
 // http://127.0.0.1:5001/api/appointments
 
 // {
-//     "patientId": "666aa0c3079a126e2a6825b7",
+//     "patientId": "666aa0c3079a126e2a6825b7",//This is patientid
 //     "doctorId": "666ad77d66d4f874dde3c9ee",
 //     "appointmentDateTime": "2024-06-20T15:30:00Z",
 //     "status": "Accepted"
